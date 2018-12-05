f5a9dcc09acee63680bc2e01da1beb09f06b94ad
Colin Cheng <zbinlin@gmail.com>
2018-12-05 14:03:27 +0800

    {FEAT}: supports IPv6

--- clean.sh
+++ clean.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 PRIORITY=${PRIORITY:-1024}
-while ip rule delete priority ${PRIORITY} 2>/dev/null;
+
+# IPv4
+while ip -4 rule delete priority ${PRIORITY} 2>/dev/null;
+do
+	true;
+done
+
+# IPv6
+while ip -6 rule delete priority ${PRIORITY} 2>/dev/null;
 do
 	true;
 done
