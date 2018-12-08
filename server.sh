64d72e5ebe31a4e80e596d79b59f6cc69ec7856f
Colin Cheng <zbinlin@gmail.com>
2018-12-08 18:02:27 +0800

    {CHORE}: update some scripts

--- server.sh
+++ server.sh
@@ -1,7 +1,13 @@
-sysctl net.ipv4.ip_forward=1
+sysctl -w net.ipv4.ip_forward=1
 iptables -t nat -A POSTROUTING -o $I -j MASQUERADE
-iptables -t filter -A FORWARD -i $wg -j ACCEPT
-iptables -t filter -A FORWARD -o $wg -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -t filter -A FORWARD -i wg+ -j ACCEPT
+iptables -t filter -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+
+sysctl -w net.ipv6.conf.all.accept_ra=2 net.ipv6.conf.default.accept_ra=2 net.ipv6.conf.all.forwarding=1 net.ipv6.conf.default.forwarding=1
+ip6tables -t nat -A POSTROUTING -o $I -j MASQUERADE
+ip6tables -t filter -A FORWARD -i wg+ -j ACCEPT
+ip6tables -t filter -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+
 
 firewall-cmd --zone=public --add-port=853/upd --permanent
 firewall-cmd --zone=public --add-port=8443/tcp --permanent
