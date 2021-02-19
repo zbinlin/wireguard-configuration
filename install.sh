70a8d41764704d5898d549646ba7908e0204f1c5
Colin Cheng <zbinlin@gmail.com>
2021-02-19 16:42:05 +0800

    FEAT{install}: improves install for debian

--- install.sh
+++ install.sh
@@ -1,5 +1,25 @@
 #!/bin/sh -e
 
+function install_for_debian() {
+	code_name=$(lsb_release -cs)
+	target_release=stable
+	case "${code_name}" in
+		'buster' )
+			echo 'deb http://deb.debian.org/debian buster-backports main' >> /etc/apt/sources.list.d/backports.list
+			target_release=buster-backports
+			;;
+		'stretch' )
+			printf 'deb http://deb.debian.org/debian/ unstable main\n' > /etc/apt/sources.list.d/unstable.list
+			printf 'Package: *\nPin: release a=unstable\nPin-Priority: 150\n' > /etc/apt/preferences.d/limit-unstable
+			target_release=unstable
+			;;
+		* )
+			;;
+	esac
+	apt update
+	apt -t $target_release install -yq wireguard
+}
+
 if ! command -v wg >/dev/null 2>&1;
 then
 	echo 'Wireguard not found!';
@@ -7,10 +27,7 @@ then
 	case "${dist}" in
 		'Debian' )
 			echo 'Installing...'
-			printf 'deb http://deb.debian.org/debian/ unstable main\n' > /etc/apt/sources.list.d/unstable.list
-			printf 'Package: *\nPin: release a=unstable\nPin-Priority: 150\n' > /etc/apt/preferences.d/limit-unstable
-			apt update
-			apt install -yq wireguard
+			install_for_debian
 			;;
 		'Arch' )
 			echo 'Installing...'
