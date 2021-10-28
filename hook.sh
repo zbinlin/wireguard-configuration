f8f66a2a2015fe21b078e7ee074232008ed45be1
Colin Cheng <zbinlin@gmail.com>
2021-10-28 21:26:17 +0800

    FEAT: adds nftables version (apply rule fast than ip-rule version)

---
+++ hook.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+case $1 in
+    (up)
+        nft -f ./domestic.nft || true
+        ip -4 route add 0.0.0.0/0 dev $WG_DEV table $FWMARK
+        ip -6 route add ::/0 dev $WG_DEV table $FWMARK
+        ip -4 rule add fwmark $FWMARK table $FWMARK
+        ip -6 rule add fwmark $FWMARK table $FWMARK
+        ip -4 rule add table main suppress_prefixlength 0
+        ip -6 rule add table main suppress_prefixlength 0
+        ip -4 rule add to $ENDPOINT table main || ip -6 rule add to $ENDPOINT table main
+        ;;
+    (down)
+        ip -4 rule delete to $ENDPOINT table main || ip -6 rule delete to $ENDPOINT table main
+        ip -6 rule delete table main suppress_prefixlength 0
+        ip -4 rule delete table main suppress_prefixlength 0
+        ip -6 rule delete fwmark $FWMARK table $FWMARK
+        ip -4 rule delete fwmark $FWMARK table $FWMARK
+        nft delete table inet wg.domestic || true
+        ;;
+    (*)
+        echo "Usage: ./hook.sh [up|down]"
+        exit 1
+        ;;
+esac
